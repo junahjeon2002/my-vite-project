0e503cfe0555aeb01223463eb3f32ded1ff87c19
junahjeon2002 <junahjeon2002@snu.ac.kr>
2025-04-12 18:00:49 +0900

    '

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-}) 
+  define: {
+    // Node.js 환경에서는 getRandomValues를 대체
+    'global.crypto': 'require("crypto")'
+  },
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify'
+    }
+  }
+})
